78cb8de1d74607c0503546a3d6cbd92b017aeb01
weimu1997 <595877155@qq.com>
2019-04-07 17:17:36 +0800

    20190407

---
+++ 19_01_25_22/app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from app.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = "login"
+login.login_message = "请登录后查看页面"
+bootstarp = Bootstrap(app)
+
+from app import routes, models
---
+++ 19_01_25_22/app/routes.py
@@ -0,0 +1,150 @@
+from app import app, db
+from app.models import User, Post
+from app.forms import PostForm, LoginForm, RegistrationForm, EditProfileForm
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import logout_user, login_required, current_user, login_user
+from werkzeug.urls import url_parse
+from datetime import datetime
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
+@login_required
+def index():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash("微博已发表")
+        return redirect(url_for("index"))
+    page = request.args.get("page", 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app.config["POSTS_PER_PAGE"], False)
+    next_url = url_for(
+        "index", page=posts.next_num) if posts.has_next else None
+    prev_url = url_for(
+        "index", page=posts.prev_num) if posts.has_prev else None
+    return render_template("index.html", title="个人主页", form=form, posts=posts.items, next_url=next_url, prev_url=prev_url)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("用户名或密码错误")
+            return redirect(url_for("login"))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get("next")
+        if not next_page or url_parse(next_page).netloc != "":
+            next_page = url_for("index")
+        return redirect(next_page)
+    return render_template("login.html", title="登录", form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("已成功注册")
+        return redirect(url_for("login"))
+    return render_template("register.html", title="注册", form=form)
+
+
+@app.route("/user/<username>")
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    page = request.args.get("page", 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config["POSTS_PER_PAGE"], False)
+    next_url = url_for("user", username=user.username,
+                       page=posts.next_num) if posts.has_next else None
+    prev_url = url_for("user", username=user.username,
+                       page=posts.prev_num) if posts.has_prev else None
+    return render_template("user.html", title="主页", user=user, posts=posts.items, next_url=next_url, prev_url=prev_url)
+
+
+@app.route("/edit_profile", methods=["GET", "POST"])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash("信息已更新")
+        return redirect(url_for("edit_profile"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template("edit_profile.html", title="修改个人信息", form=form)
+
+
+@app.route("/explore")
+@login_required
+def explore():
+    page = request.args.get("page", 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config["POSTS_PER_PAGE"], False)
+    next_url = url_for("explore", page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for("explore", page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template("explore.html", title="热点", posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
+
+
+@app.route("/follow/<username>")
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash("用户{}不存在".format(username))
+        return redirect(url_for("index"))
+    if user == current_user:
+        flash("只能关注另一位用户")
+        return redirect(url_for("user", username=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash("已成功关注用户{}".format(username))
+    return redirect(url_for("user", username=username))
+
+
+@app.route("/unfollow/<username>")
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash("用户{}不存在".format(username))
+        return redirect(url_for("index"))
+    if user == current_user:
+        flash("只能取消关注另一位用户")
+        return redirect(url_for("user", username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash("已取消关注用户{}".format(username))
+    return redirect(url_for("user", username=username))
